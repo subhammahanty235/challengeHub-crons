b9ada0b86e5d6c2177a9e52478bbe9d50eec2194
subhammahanty235 <subhammahanty235@gmail.com>
2024-01-08 00:50:08 +0530

    base of cron jobs created

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const express = require("express");
 app = express(); // Initializing app 
   
 // Creating a cron job which runs on every 10 second 
-cron.schedule("*/60 * * * * *", function() { 
-    console.log(new Date().toLocaleTimeString())
-    console.log("running a task every 10 second"); 
+cron.schedule("0 * * * *", function() { 
+    console.log("running a task every hour"); 
 }); 
-cron.schedule('40 00 * * *', () => {
+cron.schedule('30 18 * * *', () => {   //UTC time (IST: 00:00 am)
     // Your code to be executed goes here
-    console.log(new Date().toLocaleDateString());
-    console.log('Cron job executed at around 12:30 AM');
+    console.log('Cron job executed at around 00:00 AM');
   });
+
+cron.schedule('0 12 * * *' ,()=>{    //UTC time (IST: 17:30 pm)
+    console.log('cron job executed at 5:30 AM')
+})
   
 app.listen(3000); 
