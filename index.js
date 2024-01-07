8e1fde83e111b23c7b69c918b0207eddf1dc966f
subhammahanty235 <subhammahanty235@gmail.com>
2024-01-07 23:44:40 +0530

    12 am cron job added - test

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const express = require("express");
 app = express(); // Initializing app 
   
 // Creating a cron job which runs on every 10 second 
-cron.schedule("*/10 * * * * *", function() { 
+cron.schedule("*/60 * * * * *", function() { 
     console.log("running a task every 10 second"); 
 }); 
+cron.schedule('0 0 * * *', () => {
+    // Your code to be executed goes here
+    console.log('Cron job executed at 12 AM');
+  });
   
 app.listen(3000); 
