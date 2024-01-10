d095897e96bfbe045f25ff6ed34a3f6a06dd0dfc
subhammahanty235 <subhammahanty235@gmail.com>
2024-01-10 17:44:18 +0530

    testing cron

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const cron = require("node-cron"); 
 const express = require("express"); 
-  
+const axios = require("axios");
+
 app = express(); // Initializing app 
   
 // Creating a cron job which runs on every 10 second 
-cron.schedule("0 * * * *", function() { 
+cron.schedule("* * * * *", function() { 
     console.log("running a task every hour"); 
 }); 
-cron.schedule('30 18 * * *', () => {   //UTC time (IST: 00:00 am)
+cron.schedule('30 18 * * *', async() => {   //UTC time (IST: 00:00 am)
     // Your code to be executed goes here
+    
     console.log('Cron job executed at around 00:00 AM');
   });
 
-cron.schedule('0 12 * * *' ,()=>{    //UTC time (IST: 17:30 pm)
-    console.log('cron job executed at 5:30 AM')
+cron.schedule('20 12 * * *' ,()=>{    
+
+    console.log('cron job executed at 5:30 PM')
 })
   
 app.listen(3000); 
